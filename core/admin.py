89b2ff840bdb5d0b7c02fdcd68517e31e5053aa7
Htet Paing Win <htetpaingwin1999@gmail.com>
2024-01-11 20:09:48 +0630

    2024/1/11 commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+
+
+@admin.register(Student)
+class StudentAdmins(admin.ModelAdmin):
+    list_display = ['id', 'name', 'roll', 'city']
